55eb473e6f37496bf529ff6c016fdd3e20903d35
Vivek Soni <sonivivek2007@gmail.com>
2026-02-02 18:53:34 +0530

    initiate

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("ShowBuzz Server Running 🚀");
+});
+
+app.listen(3000, () => console.log("Server started on 3000"));
